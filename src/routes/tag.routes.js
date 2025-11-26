98f64580e6efb0f5cf76d1eb7ba759bf4fdec33b
Ramiro <ramiroroman306@gmail.com>
2025-11-26 19:17:25 -0300

    Se terminaron las rutas de tag

--- src/routes/tag.routes.js
+++ src/routes/tag.routes.js
@@ -6,13 +6,17 @@ import {
   updateTag,
   deleteTag,
 } from "../controllers/tag.controllers.js";
+import { createNewTagValidations, deleteTagValidations, getAllTagsValidations, getTagByIdValidations, updateTagValidations } from "../middlewares/validations/tag.validator.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
+import { applyValidations } from "../middlewares/validator.js";
+import { adminMiddleware } from "../middlewares/admin.middleware.js";
 
 const tagRouter = Router();
 
-tagRouter.post("/tags", createNewTag);
-tagRouter.get("/tags", getAllTags);
-tagRouter.get("/tags/:id", getTagById);
-tagRouter.put("/tags/:id", updateTag);
-tagRouter.delete("/tags/:id", deleteTag);
+tagRouter.post("/tags", authMiddleware, adminMiddleware, createNewTagValidations, applyValidations, createNewTag);
+tagRouter.get("/tags", authMiddleware, getAllTagsValidations, applyValidations, getAllTags);
+tagRouter.get("/tags/:id",authMiddleware,adminMiddleware,getTagByIdValidations, applyValidations, getTagById);
+tagRouter.put("/tags/:id", authMiddleware, adminMiddleware, updateTagValidations, applyValidations, updateTag);
+tagRouter.delete("/tags/:id", authMiddleware, adminMiddleware, deleteTagValidations, applyValidations, deleteTag);
 
 export default tagRouter;
