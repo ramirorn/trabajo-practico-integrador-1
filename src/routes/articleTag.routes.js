4fdc817901db6f7d29b0fcfd2d78987e09c4dff5
Ramiro <ramiroroman306@gmail.com>
2025-11-26 19:30:01 -0300

    Correccion en las rutas ArticleTag

--- src/routes/articleTag.routes.js
+++ src/routes/articleTag.routes.js
@@ -6,13 +6,17 @@ import {
   updateArticleTag,
   deleteArticleTag,
 } from "../controllers/articleTag.controllers.js";
+import { ownerOrAdminMiddleware } from "../middlewares/ownerOrAdmin.middleware.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
+import { createNewArticleTagValidations, deleteArticleTagByIdValidations, getAllArtcileTagValidations, getArticleTagByIdValidations, updateArticleTagValidations } from "../middlewares/validations/article_tag.validator.js";
+import { applyValidations } from "../middlewares/validator.js";
 
 const articleTagRouter = Router();
 
 articleTagRouter.post("/articles-tags", createNewArticleTag);
 articleTagRouter.get("/articles-tags", getAllArticleTag);
 articleTagRouter.get("/articles-tags/:id", getArticleTagById);
-articleTagRouter.put("/articles-tags/:id", updateArticleTag);
-articleTagRouter.delete("/articles-tags/:id", deleteArticleTag);
+articleTagRouter.put("/articles-tags/:id", authMiddleware, ownerOrAdminMiddleware, updateArticleTagValidations, updateArticleTag);
+articleTagRouter.delete("/articles-tags/:id", authMiddleware, ownerOrAdminMiddleware, deleteArticleTagByIdValidations, deleteArticleTag);
 
 export default articleTagRouter;
