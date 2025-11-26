ecd32e983a05cea96255647d2d596a372f4b9142
Ramiro <ramiroroman306@gmail.com>
2025-11-26 19:12:22 -0300

    Correccion rutas auth

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -7,10 +7,12 @@ import {
   updateAuthProfile,
 } from "../controllers/auth.controllers.js";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
+import { createNewUserValidations, updateUserValidations } from "../middlewares/validations/user.validator.js";
+import { applyValidations } from "../middlewares/validator.js";
 
 export const authRouter = Router();
-authRouter.post("/auht/register", register);
-authRouter.post("/auht/login", login);
-authRouter.post("/auht/logout", logout);
-authRouter.get("/auht/profile", authMiddleware, getAuthProfile);
-authRouter.put("/auht/profile", authMiddleware, updateAuthProfile);
+authRouter.post("/auth/register", createNewUserValidations, applyValidations, register);
+authRouter.post("/auth/login", login);
+authRouter.post("/auth/logout", authMiddleware, logout);
+authRouter.get("/auth/profile", authMiddleware, getAuthProfile);
+authRouter.put("/auth/profile", authMiddleware, updateUserValidations, applyValidations, updateAuthProfile);
